dc8f6ba9a3e2c6e8897ba93e30d1fcca4a49ad7e
vstokstad <v.stokstad@gmail.com>
2021-07-15 14:25:56 +0200

    added recaptcha to dep. and WIP on the form. getform.io

--- src/components/landing/Contact/ContactForm/index.jsx
+++ src/components/landing/Contact/ContactForm/index.jsx
@@ -6,48 +6,48 @@ import { Error, Center, InputField } from './styles';
 
 const ContactForm = ({ setFieldValue, isSubmitting, values, errors, touched }) => (
   <Form
-    name="Contact"
-    method="POST"
-    action="https://getform.io/f/570b4aa7-9d04-490f-a87f-6c5d42c68720"
+    name='Contact'
+    method='POST'
+    action='https://getform.io/f/570b4aa7-9d04-490f-a87f-6c5d42c68720'
   >
     <InputField>
       <Input
         as={FastField}
-        type="text"
-        name="name"
-        component="input"
-        aria-label="name"
-        placeholder="Full name*"
+        type='text'
+        name='name'
+        component='input'
+        aria-label='name'
+        placeholder='Full name*'
         error={touched.name && errors.name}
       />
-      <ErrorMessage component={Error} name="name" />
+      <ErrorMessage component={Error} name='name' />
     </InputField>
     <InputField>
       <Input
-        id="email"
-        aria-label="email"
-        component="input"
+        id='email'
+        aria-label='email'
+        component='input'
         as={FastField}
-        type="email"
-        name="email"
-        placeholder="Email*"
+        type='email'
+        name='email'
+        placeholder='Email*'
         error={touched.email && errors.email}
       />
-      <ErrorMessage component={Error} name="email" />
+      <ErrorMessage component={Error} name='email' />
     </InputField>
     <InputField>
       <Input
         as={FastField}
-        component="textarea"
-        aria-label="message"
-        id="message"
-        rows="8"
-        type="text"
-        name="message"
-        placeholder="Message*"
+        component='textarea'
+        aria-label='message'
+        id='message'
+        rows='8'
+        type='text'
+        name='message'
+        placeholder='Message*'
         error={touched.message && errors.message}
       />
-      <ErrorMessage component={Error} name="message" />
+      <ErrorMessage component={Error} name='message' />
     </InputField>
 
     {values.success && (
@@ -58,7 +58,8 @@ const ContactForm = ({ setFieldValue, isSubmitting, values, errors, touched }) =
       </InputField>
     )}
     <Center>
-      <Button secondary type="submit" name="submit" disabled={isSubmitting}>
+      <Input type='hidden' id='captchaResponse' name='g-recaptcha-response' />
+      <Button secondary type='submit' name='submit' disabled={isSubmitting}>
         Submit
       </Button>
     </Center>
@@ -78,7 +79,13 @@ export default withFormik({
       email: Yup.string().email('Invalid email').required('Email field is required'),
       message: Yup.string().required('Message field is required'),
     }),
-  handleSubmit: async ({ name, email, message,}, { setSubmitting, resetForm, setFieldValue }) => {
+  handleSubmit: async ({ name, email, message }, { setSubmitting, resetForm, setFieldValue }) => {
+    grecaptcha.ready(function() {
+      grecaptcha.execute('6LftVPUZAAAAAIwqTEu14G3I4gUVmE33M_HoFZLW', {action: 'homepage'})
+        .then(function(token) {
+          document.getElementById('captchaResponse').value = token;
+        });
+    });
     try {
       const encode = (data) =>
         Object.keys(data)
@@ -94,14 +101,14 @@ export default withFormik({
           email,
           message,
         }),
-      }).then(r=>console.log("Success form send."));
+      }).then(r => console.log('Success form send.'));
       await setSubmitting(false);
       await setFieldValue('success', true);
       setTimeout(() => resetForm(), 2000);
     } catch (err) {
       setSubmitting(false);
       setFieldValue('success', false);
-			alert('Something went wrong, please try again!') // eslint-disable-line
+      alert('Something went wrong, please try again!'); // eslint-disable-line
     }
   },
 })(ContactForm);
